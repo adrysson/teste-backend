4372f2ea7826e9ab2daeffd53792fcd5a0cdc62f
Adrysson <adrysson_santos@msn.com>
2019-02-01 03:13:07 -0300

    Organizando código

--- api/src/Controller/V1/Statistics/StudentsController.php
+++ api/src/Controller/V1/Statistics/StudentsController.php
@@ -15,37 +15,37 @@ class StudentsController extends AppController
 
     public function keep_studying()
     {
-        $students = $this->Students->Answers->find('all', [
-            'contain' => ['Students']
-        ])->count();
+        $students = $this->Students->Answers->find('all', ['contain' => 'Students'])->count();
 
-        $students_studyng = $this->Students->Answers->find('all', [
-            'contain' => ['Students'],
+        $students_studying = $this->Students->Answers->find('all', [
+            'contain' => 'Students',
             'conditions' => ['alternative_id' => 37],
         ])->count();
 
-        $national = round($students_studyng/$students*100, 4);
+        $national = round($students_studying/$students*100, 4);
 
         $query = $this->Students->find('all');
         $regionals = $query
-        ->select([
-            'description' => 'regional',
-            'average' => $query->func()->count('*')
-        ])
-        ->group(['regional']);
+            ->select([
+                'description' => 'regional',
+                'average' => $query->func()->count('*')
+            ])
+            ->group(['regional'])
+            ->formatResults(function (\Cake\Collection\CollectionInterface $results) {
+                // iterando o resultado e calculando as médias por estado
+                return $results->map(function ($row) {
+                     $regional_studying = $this->Students->find('all', [
+                            'conditions' => ['regional' => $row['description']],
+                        ])
+                        ->matching('Answers', function ($q) {
+                            return $q->where(['Answers.alternative_id ' => 37]);
+                        })->count();
 
-        $query->formatResults(function (\Cake\Collection\CollectionInterface $results) {
-            return $results->map(function ($row) {
-                $row['average'] = $this->Students->find('all', [
-                    'conditions' => ['regional' => $row['description']],
-                ])
-                ->matching('Answers', function ($q) {
-                    return $q->where(['Answers.alternative_id ' => 37]);
-                })
-                ->count() / $row['average'] * 100;
-                return $row;
+                    $row['average'] = round($regional_studying/$row['average'] * 100, 4);
+
+                    return $row;
+                });
             });
-        });
         $this->set([
             'regionals' => $regionals,
             'national' => $national,
